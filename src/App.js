cc008336bf88c8060c3c014f447747b1077d2ab1
frastsytt <frastsytt@gmail.com>
2022-08-06 01:48:41 +0300

    countdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const currentTime = new Date().getTime();
+  const targetTime = new Date(new Date().getFullYear(), 11, 24, 0, 0, 0, 0).getTime();
+  const [leftTime, setLeftTime] = useState(new Date(targetTime - currentTime).getTime())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLeftTime(leftTime => leftTime - 1000);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      <h1>Christmas eve countdown </h1>
+      <div className='DateBlock'>
+        <div className='DateElementValue'>{new Date(leftTime).getMonth()}
+          <p className='DateElementDesc'>Months</p>
+          </div>
+        <div className='DateElementValue'>{new Date(leftTime).getDate()}
+          <p className='DateElementDesc'>Days</p>
+          </div>
+        <div className='DateElementValue'>{new Date(leftTime).getHours()}
+          <p className='DateElementDesc'>Hours</p>
+          </div>
+        <div className='DateElementValue'>{new Date(leftTime).getMinutes()}
+          <p className='DateElementDesc'>Minutes</p>
+          </div>
+        <div className='DateElementValue'>{new Date(leftTime).getSeconds()}
+          <p className='DateElementDesc'>Seconds</p>
+          </div>
+      </div>
     </div>
   );
 }
